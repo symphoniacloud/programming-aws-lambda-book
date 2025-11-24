3aefac220beb087db04eb1b46f47f39a8aadc2cf
Mike Roberts <mike@symphonia.io>
2025-11-24 16:02:33 -0500

    Poll for messages

--- chapter6/integration-tests/src/test/java/book/pipeline/PipelineIT.java
+++ chapter6/integration-tests/src/test/java/book/pipeline/PipelineIT.java
@@ -12,6 +12,7 @@ import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.File;
 import java.nio.file.Paths;
+import java.time.Duration;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -45,10 +46,9 @@ public class PipelineIT {
                 .key(key)
                 .build(), Paths.get(file.toURI()));
 
-        // 2. Check for executions of SingleEventLambda
-        Thread.sleep(30000);
+        // 2. Check for executions of SingleEventLambda with retry logic
         var singleEventLambda = resolvePhysicalId("SingleEventLambda");
-        var logMessages = getLogMessages(singleEventLambda);
+        var logMessages = pollForLogMessages(singleEventLambda, 3, Duration.ofMinutes(2));
         assertThat(logMessages, hasItems(
                 "WeatherEvent{locationName='Brooklyn, NY', temperature=91.0, timestamp=1564428897, longitude=-73.99, latitude=40.7}",
                 "WeatherEvent{locationName='Oxford, UK', temperature=64.0, timestamp=1564428898, longitude=-1.25, latitude=51.75}",
@@ -83,24 +83,52 @@ public class PipelineIT {
     private Set<String> getLogMessages(String lambdaName) {
         var logGroup = getLogGroup(lambdaName);
 
-        return logs.describeLogStreams(DescribeLogStreamsRequest.builder()
-                        .logGroupName(logGroup)
-                        .build())
-                .logStreams().stream()
-                .map(LogStream::logStreamName)
-                .flatMap(logStream -> logs.getLogEvents(GetLogEventsRequest.builder()
-                                .logGroupName(logGroup)
-                                .logStreamName(logStream)
-                                .build())
-                        .events().stream())
-                .map(OutputLogEvent::message)
-                .filter(message -> message.contains("WeatherEvent"))
-                .map(String::trim)
-                .collect(Collectors.toSet());
+        try {
+            return logs.describeLogStreams(DescribeLogStreamsRequest.builder()
+                            .logGroupName(logGroup)
+                            .build())
+                    .logStreams().stream()
+                    .map(LogStream::logStreamName)
+                    .flatMap(logStream -> logs.getLogEvents(GetLogEventsRequest.builder()
+                                    .logGroupName(logGroup)
+                                    .logStreamName(logStream)
+                                    .build())
+                            .events().stream())
+                    .map(OutputLogEvent::message)
+                    .filter(message -> message.contains("WeatherEvent"))
+                    .map(String::trim)
+                    .collect(Collectors.toSet());
+        } catch (ResourceNotFoundException e) {
+            // Log group doesn't exist yet - Lambda hasn't been invoked
+            return Set.of();
+        }
     }
 
     private String getLogGroup(String lambdaName) {
         return String.format("/aws/lambda/%s", lambdaName);
+    }
+
+    // Added this functionality to handle warmup of Lambda, etc., since otherwise was getting failures
+    private Set<String> pollForLogMessages(String lambdaName, int expectedCount, Duration timeout) throws InterruptedException {
+        var endTime = System.currentTimeMillis() + timeout.toMillis();
+        var pollInterval = Duration.ofSeconds(5);
+
+        while (System.currentTimeMillis() < endTime) {
+            var logMessages = getLogMessages(lambdaName);
+
+            if (logMessages.size() >= expectedCount) {
+                System.out.println("Found " + logMessages.size() + " log messages after polling");
+                return logMessages;
+            }
+
+            System.out.println("Found " + logMessages.size() + " log messages, waiting for " + expectedCount + "...");
+            Thread.sleep(pollInterval.toMillis());
+        }
+
+        // Return whatever we have after timeout
+        var logMessages = getLogMessages(lambdaName);
+        System.out.println("Timeout reached. Found " + logMessages.size() + " log messages");
+        return logMessages;
     }
 
 }
